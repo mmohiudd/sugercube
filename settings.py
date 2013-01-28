e0ce75ad55f2f46e96dfdf4467d9a8672fe32379
Muntasir Mohiuddin <muntasir.mohiuddin@gmail.com>
2013-01-28 15:17:46 -0500

    restrucutred for Facebook

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 SETTINGS = {
-    'app_id': "128585133977376",
-    'app_secret': "14030215d3ff974c14bdc359d2a3feff",
-    'namespace': "sugercube",
+    'facebook': {
+        'app_id': "128585133977376",
+        'app_secret': "14030215d3ff974c14bdc359d2a3feff",
+        'namespace': "sugercube",
+    }
 }
